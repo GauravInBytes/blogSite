5a2c924429024d32ca0b8187d87d182695aa4369
GauravInBytes <gode429@gmail.com>
2024-10-31 17:22:51 +0530

    added environment variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import './App.css'
+import './App.css';
+import config from './config/config';
 
 function App() {
+  console.log(config.appwriteUrl);
   return (
     <>
       <h1>Hello World</h1>
